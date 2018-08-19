af6999d9d67ee2e402528b71541ebd1bf0c5e706
rachael miller design <rachael@rachaelmillerdesign.com>
2018-08-19 17:15:54 -0400

    added initial javascript for sign in / out / up / change password

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+const api = require('./api')
+const ui = require('./ui')
+
+// ~~~~~~~~~~~~~~~~~~~~~~`
+//  FORM FIELD FUNCTIONS
+// ~~~~~~~~~~~~~~~~~~~~~~`
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  console.log('sign up ran!')
+
+  const data = getFormFields(this)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  console.log('sign in ran!')
+
+  const data = getFormFields(this)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('sign out ran')
+
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('change password ran!')
+
+  const data = getFormFields(this)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+// ~~~~~~~~~~~~~~~~~~~~~~
+// HANDLERS
+// ~~~~~~~~~~~~~~~~~~~~~~
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#sign-out').on('submit', onSignOut)
+  $('#change-password').on('submit', onChangePassword)
+}
+
+module.exports = {
+  addHandlers
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,123 @@
+'use strict'
+
+const store = require('../store')
+
+const signUpSuccess = function (data) {
+  $('#sign-out').removeClass('hidden')
+  $('#sign-up').addClass('hidden')
+  $('#change-password').removeClass('hidden')
+  $('#signUpSuccess').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#signUpSuccess').modal('hide')
+  }, 2000)
+  $('#sign-up')[0].reset()
+  console.log('signUpSuccess ran. Data is :', data)
+}
+
+const signUpFailure = function (error) {
+  $('#signInFailure').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#signUpFailure').modal({
+      show: false
+    })
+  }, 2000)
+  $('#sign-up')[0].reset()
+  console.error('signUpFailure ran. Error is :', error)
+}
+
+const signInSuccess = function (data) {
+  $('#sign-out').removeClass('hidden')
+  $('#sign-in').addClass('hidden')
+  $('#sign-up').addClass('hidden')
+  $('#change-password').removeClass('hidden')
+  $('#myId').removeClass('hidden')
+  console.log('signInSuccess ran. Data is :', data)
+  $('#signInSuccess').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#signInSuccess').modal('hide')
+  }, 2000)
+  store.user = data.user
+  console.log(store.user.id)
+  $('#sign-in')[0].reset()
+  // $('#myId').html(`<p>${store.user.id}</p>`)
+}
+
+const signInFailure = function (error) {
+  $('#signInFailure').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#signInFailure').modal('hide')
+  }, 2000)
+  $('#sign-in')[0].reset()
+  console.error('signInFailure ran. Error is :', error)
+}
+
+const signOutSuccess = function () {
+  $('#sign-in').removeClass('hidden')
+  $('#sign-up').removeClass('hidden')
+  $('#sign-out').addClass('hidden')
+  $('#change-password').addClass('hidden')
+  $('#signOutSuccess').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#signOutSuccess').modal('hide')
+  }, 2000)
+  $('#sign-out')[0].reset()
+  // console.log('signOutSuccess ran and nothing was returned!')
+  store.user = null
+}
+
+const signOutFailure = function (error) {
+  $('#signOutFailure').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#signOutFailure').modal('hide')
+  }, 2000)
+  $('#sign-out')[0].reset()
+  console.error('signOutFailure ran. Error is :', error)
+}
+
+const changePasswordSuccess = function () {
+  $('#changePasswordSuccess').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#changePasswordSuccess').modal('hide')
+  }, 2000)
+  $('#change-password')[0].reset()
+  // console.log('changePasswordSuccess ran and nothing was returned!')
+}
+
+const changePasswordFailure = function (error) {
+  $('#changePasswordFailure').modal({
+    show: true
+  })
+  setTimeout(function () {
+    $('#changePasswordFailure').modal('hide')
+  }, 2000)
+  $('#change-password')[0].reset()
+  console.error('changePasswordFailure ran. Error is :', error)
+}
+// ~~~~~~~~~~~~~~~~~~~~~~
+// MODULE EXPORTS
+// ~~~~~~~~~~~~~~~~~~~~~~
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}
