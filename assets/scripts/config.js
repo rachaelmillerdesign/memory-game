1ba980e342936c68a313287e29decbff02f24a6f
rachael miller design <rachael@rachaelmillerdesign.com>
2018-09-07 17:56:47 -0400

    added heroku http address to apiUrl

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://git.heroku.com/murmuring-brushlands-54907.git',
   development: 'http://localhost:4741'
 }
 
